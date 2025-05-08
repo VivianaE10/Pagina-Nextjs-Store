6e2036b8a6ab8eb01706dfa7d0f36b1325e98f58
Viviana <vivioescobar@gmail.com>
2025-05-08 14:49:21 -0500

    Forulario de singup

---
+++ src/actions/index.ts
@@ -0,0 +1,7 @@
+//creamos las acciones tipo servidor van a correc las funciones en el servidor sin importar que el componente este en el lado del cliente
+
+// "use server";
+
+export const handleCreateUser = (formData) => {
+  console.log("handleCreateUser", formData);
+};
